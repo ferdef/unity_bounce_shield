1332466b13270121073ba9e1212c34b98b943c01
F3RD3F <f3rd3f@gmail.com>
2022-09-25 22:01:36 +0200

    Added Auto spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject Prefab;
+    public float Speed;
+    public int Counter;
+    private Transform _transform;
+    private int _currentCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = GetComponent<Transform>();        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void FixedUpdate()
+    {
+           
+        if( _currentCounter <= 0)
+        {
+            var newBall = Instantiate(Prefab, _transform.position, Quaternion.identity);
+
+            var rigidBody = newBall.GetComponent<Rigidbody>();
+            rigidBody.AddForce(transform.right * Speed);
+            _currentCounter = Counter;
+        }
+        else
+        {
+            _currentCounter--;
+        }
         
     }
 }
